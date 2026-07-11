dbac29d553ef968fae71db863e34a8e9306d2ec7
manuelhelmerichs <manuelhelmerichs@icloud.com>
2026-07-11 11:09:26 +0200

    Move benchmarks to top level

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Pytest fixtures and environment tweaks."""
-
-import os
-
-# Force CPU execution inside tests so importing align doesn't require CUDA.
-os.environ.setdefault("JAX_PLATFORMS", "cpu")
-os.environ.setdefault("CUDA_VISIBLE_DEVICES", "")
-os.environ.setdefault("JAX_VISIBLE_DEVICES", "")
